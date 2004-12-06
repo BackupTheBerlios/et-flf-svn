0af4a6ad2a0690edc426e065609f630734bf02de
6wl <6wl@b1572683-45ea-0310-b929-a862e6382b4a>
2004-12-06 11:53:36 +0000

    First attempt at adding and removing weapons we want/need

---
+++ branch/weapon-changes1/src/botai/syn.h
@@ -0,0 +1,15 @@
+//===========================================================================
+//
+// Name:			syn.h
+// Function:		synonyms
+//
+//===========================================================================
+
+#define CONTEXT_ALL						0xFFFFFFFF
+#define CONTEXT_NORMAL					1
+#define CONTEXT_NEARBYITEM				2
+#define CONTEXT_CTFREDTEAM				4
+#define CONTEXT_CTFBLUETEAM				8
+#define CONTEXT_REPLY					16
+
+#define CONTEXT_NAMES 1024
